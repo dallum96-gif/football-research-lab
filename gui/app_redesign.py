655d03d6ff6e207cbab90342a44a50c4e60c5b49
dallum96-gif <d.allum96@gmail.com>
2026-08-15 17:54:54 +0100

    Use locked Streamlit sidebar for fixed navigation

--- gui/app_redesign.py
+++ gui/app_redesign.py
@@ -19,7 +19,7 @@ st.set_page_config(
     page_title="Football Research Lab",
     page_icon="⚽",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="locked",
 )
 
 apply_theme()
