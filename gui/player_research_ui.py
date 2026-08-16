258d2b1df8bf791b7aaa5df158cbc6d4cf851fae
dallum96-gif <d.allum96@gmail.com>
2026-08-16 21:13:44 +0100

    ui: fix player detail xA rate key

--- gui/player_research_ui.py
+++ gui/player_research_ui.py
@@ -450,7 +450,7 @@ def render_player_research_ui():
             ("Goals / 90", fmt(player["goals_per_90"], 3)),
             ("Assists / 90", fmt(player["assists_per_90"], 3)),
             ("xG / 90", fmt(player["xg_per_90"], 3)),
-            ("xA / 90", fmt(player["xA_per_90"], 3)),
+            ("xA / 90", fmt(player["xa_per_90"], 3)),
             ("BPS / 90", fmt(player["bps_per_90"], 3)),
         ]
         for col, (label, value) in zip(rates, rate_values):
