bdb00382ff8f2631c184f91e708276ec49ff4d4a
dallum96-gif <d.allum96@gmail.com>
2026-08-18 16:40:15 +0100

    Route Team Research through expanded stats renderer

--- gui/team_research_ui.py
+++ gui/team_research_ui.py
@@ -1,5 +1,5 @@
 """Compatibility shim for the current FRL Team Research workspace."""
 
-from gui.team_research_ui_v7 import render_team_research_ui
+from gui.team_research_ui_v8 import render_team_research_ui
 
 __all__ = ["render_team_research_ui"]
