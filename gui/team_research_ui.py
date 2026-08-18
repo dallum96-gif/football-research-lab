95821e94bcb507ee401b41415d7bd18f472e892a
dallum96-gif <d.allum96@gmail.com>
2026-08-18 16:02:57 +0100

    refactor: promote Team Research v6 renderer

--- gui/team_research_ui.py
+++ gui/team_research_ui.py
@@ -1,5 +1,5 @@
 """Compatibility shim for the current FRL Team Research workspace."""
 
-from gui.team_research_ui_v5 import render_team_research_ui
+from gui.team_research_ui_v6 import render_team_research_ui
 
 __all__ = ["render_team_research_ui"]
