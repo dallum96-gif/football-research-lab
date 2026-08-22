ceae5b7b1becced7c286a9b6b457ff2760b8ac4f
dallum96-gif <d.allum96@gmail.com>
2026-08-22 18:42:46 +0100

    test source-field taxonomy exceptions

--- tests/test_source_field_taxonomy.py
+++ tests/test_source_field_taxonomy.py
@@ -14,9 +14,17 @@ def test_taxonomy_does_not_change_registry_status():
 
 
 def test_representative_navigation_categories():
-    assert classify_field("player_season", "goals") [0] == "Shooting & Finishing"
+    assert classify_field("player_season", "goals")[0] == "Shooting & Finishing"
     assert classify_field("player_season", "duelsWon")[0] == "Duels & Aerials"
     assert classify_field("player_season", "successfulLongPasses")[0] == "Passing & Distribution"
     assert classify_field("player_season", "successfulDribbles")[0] == "Dribbling & Carrying"
     assert classify_field("player_match", "yellowCards")[0] == "Discipline"
     assert classify_field("player_match", "saves")[0] == "Goalkeeping"
+
+
+def test_explicit_unclassified_exceptions_are_placed():
+    assert classify_field("player_match", "penaltyConceded")[0] == "Discipline"
+    assert classify_field("player_match", "penaltyFaced")[0] == "Discipline"
+    assert classify_field("player_match", "penaltyMiss")[0] == "Shooting & Finishing"
+    assert classify_field("player_match", "penaltyWon")[0] == "Chance Creation"
+    assert classify_field("player_match", "substitute.1")[0] == "Playing Time"
