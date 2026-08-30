7434b2889e9d32cd3761a75301eec30655f87d9e
dallum96-gif <d.allum96@gmail.com>
2026-08-30 20:32:35 +0100

    Register Team Stats league rankings router

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,8 @@
 from . import frl_api as _frl_api
 from .player_performance import router as player_performance_router
 from .fixture_evidence import router as fixture_evidence_router
+from .team_stats_rankings import router as team_stats_rankings_router
 
 _frl_api.app.include_router(player_performance_router)
 _frl_api.app.include_router(fixture_evidence_router)
+_frl_api.app.include_router(team_stats_rankings_router)
