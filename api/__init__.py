dcf222ff832758b2eb9ee9bd2bd4d186f99dbb00
dallum96-gif <d.allum96@gmail.com>
2026-08-31 22:43:18 +0100

    feat: register player stats API router

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,9 @@ from . import frl_api as _frl_api
 from .player_performance import router as player_performance_router
 from .fixture_evidence import router as fixture_evidence_router
 from .team_stats_rankings import router as team_stats_rankings_router
+from .player_stats import router as player_stats_router
 
 _frl_api.app.include_router(player_performance_router)
 _frl_api.app.include_router(fixture_evidence_router)
 _frl_api.app.include_router(team_stats_rankings_router)
+_frl_api.app.include_router(player_stats_router)
