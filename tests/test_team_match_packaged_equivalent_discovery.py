3529e0332a3b4822dad6b58947c2492bde1d3526
dallum96-gif <d.allum96@gmail.com>
2026-09-04 22:28:56 +0100

    test: reject semantic false friends in route discovery

--- tests/test_team_match_packaged_equivalent_discovery.py
+++ tests/test_team_match_packaged_equivalent_discovery.py
@@ -24,6 +24,7 @@ def test_close_name_is_suggested_but_not_promoted():
     assert row['best_candidate'] == 'totalLongBalls'
     assert row['best_score'] > 0.8
     assert row['route_discovery_lane'] == 'STRONG_NAME_SIMILARITY_REVIEW'
+    assert row['semantic_guard_status'] == 'QUALIFIER_GUARD_APPLIED'
     assert 'does not establish' in row['governance_note']
 
 
@@ -35,3 +36,25 @@ def test_weak_name_match_stays_fail_closed():
     row = result[0]
 
     assert row['route_discovery_lane'] == 'NO_STRONG_PACKAGED_EQUIVALENT_BY_NAME'
+
+
+def test_generic_goals_conceded_does_not_match_penalty_goals_conceded():
+    result = suggest_equivalents(
+        [{'source_field': 'goalsConceded', 'reconciliation_status': 'RAW_SNAPSHOT_ONLY'}],
+        ['penGoalsConceded', 'totalPass'],
+    )
+    row = result[0]
+
+    assert row['best_candidate'] != 'penGoalsConceded'
+    assert row['semantic_conflict_candidates_rejected'] >= 1
+
+
+def test_headed_goal_does_not_match_penalty_goal_or_own_goal():
+    result = suggest_equivalents(
+        [{'source_field': 'attHdGoal', 'reconciliation_status': 'RAW_SNAPSHOT_ONLY'}],
+        ['attPenGoal', 'attIboxOwnGoal', 'headedGoals'],
+    )
+    row = result[0]
+
+    assert row['best_candidate'] == 'headedGoals'
+    assert row['semantic_conflict_candidates_rejected'] >= 2
