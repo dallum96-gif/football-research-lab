3ca90af70e160d239135c91273c78b383338433f
dallum96-gif <d.allum96@gmail.com>
2026-09-08 16:44:12 +0100

    feat: switch matchday to stat-pack workspace

--- web/src/app/matchday/[season]/[fixtureId]/page.tsx
+++ web/src/app/matchday/[season]/[fixtureId]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { AppShell } from "@/components/AppShell";
-import { MatchdayWorkspaceV3 } from "./MatchdayWorkspaceV3";
+import { MatchdayStatPackV4 } from "./MatchdayStatPackV4";
 
 const API_BASE = (process.env.NEXT_PUBLIC_FRL_API_URL ?? "http://127.0.0.1:8000").replace(/\/$/, "");
 
@@ -32,7 +32,7 @@ export default async function MatchdayFixturePage({ params }: MatchdayPageProps)
 
   return (
     <AppShell>
-      <MatchdayWorkspaceV3
+      <MatchdayStatPackV4
         key={`${season}-${fixtureId}`}
         pack={pack}
         fixtureOptions={fixtures?.fixtures ?? []}
